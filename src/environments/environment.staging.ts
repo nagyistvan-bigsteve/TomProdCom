354f1346d0c3e19f415d3fd93041b33f1702c8d6
Istvan Nagy <istvannagy8000@gmail.com>
2026-08-28 13:32:54 +0300

    fix firebase staging

--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://oislmdrtantznrhbjbrh.supabase.co',
   supabaseAnonKey:
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pc2xtZHJ0YW50em5yaGJqYnJoIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc4NzkwMzE3NCwiZXhwIjoyMTAzNDc5MTc0fQ.HIgh6CAjaEob088J_J3taeJ_7sY5i9b2YAbd8Yrud-w',
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9pc2xtZHJ0YW50em5yaGJqYnJoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc5MDMxNzQsImV4cCI6MjEwMzQ3OTE3NH0.dYjd_MhB7LQCW2fB5C-qh-yud2s6p6-FQ0jQS8uDzM8',
 };
